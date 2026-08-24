777397e3bb36848553cff3a8e05466a218a8f9c3
Akash <akashpd390@gmail.com>
2026-08-24 14:30:19 +0530

    Add: forground notification

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true 
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -40,7 +41,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+    dependencies {
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    }
 }
+
 
 flutter {
     source = "../.."
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:test_notification/firebase_options.dart';
 import 'package:test_notification/notification_service.dart';
 
-// final localNotifictionsPlugin = FlutterLocalNotificationsPlugin();
+final localNotifictionsPlugin = FlutterLocalNotificationsPlugin();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,29 +16,29 @@ Future<void> main() async {
 
   await notificationService.init();
 
-  // await initiliseLocalNotification();
+  await initiliseLocalNotification();
 
   runApp(const MyApp());
 }
 
-// Future<void> initiliseLocalNotification() async {
+Future<void> initiliseLocalNotification() async {
   
-//   const AndroidInitializationSettings androidInitSettings =
-//       AndroidInitializationSettings('@mipmap/ic_launcher');
-//   const IOSInitializationSettings iosInitSettings = IOSInitializationSettings();
-//   const InitializationSettings initSettings = InitializationSettings(
-//     android: androidInitSettings,
-//     iOS: iosInitSettings,
-//   );
+  const AndroidInitializationSettings androidInitSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const IOSInitializationSettings iosInitSettings = IOSInitializationSettings();
+  const InitializationSettings initSettings = InitializationSettings(
+    android: androidInitSettings,
+    iOS: iosInitSettings,
+  );
   
-//   await localNotifictionsPlugin.initialize(
-//     settings: initSettings,
-//     onDidReceiveNotificationResponse: (NotificationResponse response) {
-//       // Handle notification tap
-//       debugPrint('Notification payload: ${response.payload}');
-//     },
-//   );
-// }
+  await localNotifictionsPlugin.initialize(
+    settings: initSettings,
+    onDidReceiveNotificationResponse: (NotificationResponse response) {
+      // Handle notification tap
+      debugPrint('Notification payload: ${response.payload}');
+    },
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -76,31 +76,32 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
-    // FirebaseMessaging.onMessage.listen(_showForegroundNotification);
+    FirebaseMessaging.onMessage.listen(_showForegroundNotification);
   }
 
-  // Future<void> _showForegroundNotification(RemoteMessage message) async {
-  //   final data = message.data;
+  Future<void> _showForegroundNotification(RemoteMessage message) async {
+    final data = message.data;
 
-  //   await localNotifictionsPlugin.show(
-  //     id: DateTime.now().millisecondsSinceEpoch ~/ 1000,
-  //     title: data['title'] ?? message.notification?.title ?? 'Notification',
-  //     body: data['body'] ?? message.notification?.body,
-  //     notificationDetails: NotificationDetails(
-  //       android: AndroidNotificationDetails(
-  //         'channel_1',
-  //         'Channel 1',
-  //         importance: Importance.high,
-  //         priority: Priority.high,
-  //       ),
-  //       iOS: const DarwinNotificationDetails(
-  //         presentAlert: true,
-  //         presentSound: true,
-  //       ),
-  //     ),
+    await localNotifictionsPlugin.show(
+      id: DateTime.now().millisecondsSinceEpoch ~/ 1000,
+      title: data['title'] ?? message.notification?.title ?? 'Notification',
+      body: data['body'] ?? message.notification?.body,
+      notificationDetails: NotificationDetails(
+        android: AndroidNotificationDetails(
+          'channel_1',
+          'Channel 1',
+          importance: Importance.high,
+          priority: Priority.high,
+        ),
+        iOS: const DarwinNotificationDetails(
+          presentAlert: true,
+          presentSound: true,
+        ),
+      ),
+      
 
-  //   );
-  // }
+    );
+  }
 
   void _handleMessage(RemoteMessage message) {
     print(message.data);
